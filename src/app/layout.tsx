74a0d73989b9c4c71ba620e8d58ee3721f20b27c
marmalade1124 <razielrenz@gmail.com>
2026-01-15 00:39:02 +0800

    Change page title to CCNote

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,7 +3,7 @@ import Script from "next/script";
 import "./globals.css";
 
 export const metadata: Metadata = {
-  title: "Canvas Notes",
+  title: "CCNote",
   description: "Infinite canvas for your thoughts",
 };
 
