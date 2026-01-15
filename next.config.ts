aee230a55ceb43c056605e000db4bf27465326f9
marmalade1124 <razielrenz@gmail.com>
2026-01-15 15:27:47 +0800

    chore(config): Add webpack overrides for transformers.js compatibility

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,14 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  webpack: (config) => {
+    config.resolve.alias = {
+        ...config.resolve.alias,
+        "sharp$": false,
+        "onnxruntime-node$": false,
+    }
+    return config;
+  },
 };
 
 export default nextConfig;
